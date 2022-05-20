e7df6b14e7dee0cf160a2b22ddb09e7f85bea125
Carameil <bolbas.n@bk.ru>
2022-05-20 16:35:59 +0300

    add Parser library

--- app/index.php
+++ app/index.php
@@ -1,10 +1,18 @@
 <?php
 
-use App\ReadLog;
+require_once __DIR__ . '/vendor/autoload.php';
 
+use App\ReadLog;
+use BenMorel\ApacheLogParser\Parser;
 
 const PATH_TO_FILE = __DIR__ . '/logs/access_log';
-
+/*
+ * @link https://httpd.apache.org/docs/2.2/logs.html#combined
+ * */
+$logFormat = "%h %l %u %t \"%r\" %>s %b \"%{Referer}i\" \"%{User-agent}i\"";
 $reader = new ReadLog();
-
+/*
+ * @link https://github.com/BenMorel/apache-log-parser
+ * */
+$parser = new Parser($logFormat);
 
